e419228a3e90088f5076494fbe836b874ef142eb
Jayyy473 <jaden.jjoseph@gmail.com>
2025-12-02 10:22:06 -0400

    Create spice_isle.sql

---
+++ sql/spice_isle.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    role ENUM('admin','manager','staff') DEFAULT 'staff',
+    password VARCHAR(255) NOT NULL
+);
